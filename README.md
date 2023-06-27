c9c09955b00dd49665a714373d9c5b764b5b322c
gkarumba <gachegua@gmail.com>
2023-06-27 17:15:15 +0300

    [FIX] update readme file

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# odoo-pos
-A module that creates offers for products to be sold in the POS
+# odoo-pos 
+A module that creates offers for products to be sold in the POS for Odoo V15
+
+
+### 1- Install the Odoo POS Offers module
+
+### 3- Access the Offers menu item from either the Purchase Module or POS module under Products menu item
+
+### 4- Add an offer:
+1. Add the name of the offer
+2. Add the start date and End Date
+3. Add the price for the offer
+4. Add the products (```only products that can be sold```)
+5. Save and Activate the Offer
+
+### 5- Go to POS Module, products that are in the offer will have a green label
+    (Offer:<name of offer>)
+
+### 6- View the product details by clicking the info icon to view the offers details 
+    original product amount and the amount saved under the offer
+
+### 7- Pending including the Offer details in the receipt 
+    Bug on reading the data rendered to the OrderReceipt, js functionality done
--- models/offers.py
+++ models/offers.py
@@ -13,6 +13,17 @@ class ProductOffers(models.Model):
     _description = "Products Offers"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _order = "sequence"
+    
+    @api.model
+    def create(self, vals):
+        start_date = vals.get('date_start')
+        end_date = vals.get('date_stop')
+        
+        if end_date <= start_date:
+            raise ValidationError(_('You cannot set an end date equal or prior to start date'))
+        else:
+            result = super(ProductOffers, self).create(vals)
+            return result
 
     active = fields.Boolean(default=True, track_visibility='onchange')
     sequence = fields.Integer('Sequence', required=False)
@@ -20,7 +31,7 @@ class ProductOffers(models.Model):
     date_start = fields.Date('Start Date', required=True,
                              help='Starting date of Offer', track_visibility='onchange')
     date_stop = fields.Date('End Date', required=True,
-                            help='Ending Date of Offer', track_visibility='onchange')
+                            help='Ending Date of Offer',date_range="[('date', '>=', fields.Date.today())]", track_visibility='onchange')
     state = fields.Selection([('draft', 'Draft'), ('active', 'Active'), ('close', 'Closed')],
                 'State',  help='State of Offer',default='draft', track_visibility='onchange')
     price = fields.Float("Price", required=True, default=0.0, track_visibility='onchange')
@@ -37,16 +48,19 @@ class ProductOffers(models.Model):
                 record.write({
                     'state': 'active'
                 })
-                for line in record.offer_line_ids:
-                    if line.product_id:
-                        line.old_price = line.product_id.list_price
-                        line.product_id.actual_price = line.old_price
+                
+                for line in record.offer_line_ids :
+                    actual_price = self.env['product.template'].search([('id','=',line.product_id.id)]).mapped('list_price')
+                    if line.product_id and not line.product_id.on_offer:
+                        line.old_price = actual_price[0]
+                        line.product_id.actual_price = actual_price[0]
                         line.product_id.list_price = record.price
                         line.product_id.offer_price = line.offer_price
                         line.product_id.on_offer = True
                         line.product_id.product_variant_id.on_offer = True
                         line.product_id.product_variant_id.offer_id = self.id
                         line.product_id.offer_id = self.id
+                        
                         
     def set_closed(self):
         for record in self:
@@ -65,9 +79,10 @@ class ProductOffers(models.Model):
                 raise ValidationError(_('You can not edit an Offer, that has been closed'))
             else:
                 for line in record.offer_line_ids:
+                    actual_price = self.env['product.template'].search([('id','=',line.product_id.id)]).mapped('list_price')
                     if line.product_id and not line.product_id.on_offer:
-                        line.old_price = line.product_id.list_price
-                        line.product_id.actual_price = line.old_price
+                        line.old_price = actual_price[0]
+                        line.product_id.actual_price = actual_price[0]
                         line.product_id.list_price = record.price
                         line.product_id.offer_price = line.offer_price
                         line.product_id.on_offer = True
@@ -98,16 +113,21 @@ class OffersLine(models.Model):
     @api.model
     def create(self, vals):
         product_id = vals.get('product_id')
+        offer_id = vals.get('offer_id')
         actual_price = self.env['product.template'].search([('id','=',product_id)]).mapped('list_price')
-        vals.update({
-            'old_price': actual_price[0]
-        })
-        return super(OffersLine, self).create(vals)
+        offer_price = self.env['product.offer'].search([('id','=',offer_id)]).mapped('price')
+        if offer_price[0] > actual_price[0]:
+            raise ValidationError(_('You cannot set an offer price to be greater than the sales price'))
+        else:
+            vals.update({
+                'old_price': actual_price[0]
+            })
+            return super(OffersLine, self).create(vals)
 
     active = fields.Boolean(default=True, track_visibility='onchange')
     offer_id = fields.Many2one('product.offer', string='Offer', required=False,tracking=True)
     name = fields.Char('Name', required=True, readonly=True,related='product_id.name', track_visibility='onchange')
-    product_id = fields.Many2one('product.template', 'Product', required=True, domain="[('sale_ok', '=', True)]", track_visibility='onchange')
+    product_id = fields.Many2one('product.template', 'Product', required=True, domain="[('sale_ok', '=', True),('on_offer','=',False)]", track_visibility='onchange')
     old_price = fields.Float('Actual Price', required=False, readonly=True, track_visibility='onchange')
     offer_price = fields.Float('Offer Price', required=False, readonly=True, related='offer_id.price', track_visibility='onchange')
     state = fields.Selection([('draft', 'Draft'), ('active', 'Active'), ('close', 'Closed')],
